538378ad4cf9c7be025eeab6132c41452c539d81
Skalmanleif <isak.thyrel@elev.ga.ntig.se>
2021-05-07 10:56:55 +0200

    Lagt till en metod för pq formeln.

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in); //Läser in input
         String s = in.next();
+        if (s.equals("pq")) {
+            pq(in);
+        }
+    }
 
+    private static void pq(Scanner in) {
+        System.out.println("pq: p,q");
+        long p = in.nextLong();
+        long q = in.nextLong();
+        long sqrt = (long) Math.sqrt(Math.pow(-p / 2, 2) - q);
+        long x1 = ((-p/2) + sqrt);
+        long x2 = ((-p/2) - sqrt);
+        System.out.println("x1 = " + x1 + "\nx2 = " + x2);
     }
 }
