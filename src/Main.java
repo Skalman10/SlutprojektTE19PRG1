91b35c8f5a4a7ea5a419cec7f6476588f10bf4fe
Skalmanleif <isak.thyrel@elev.ga.ntig.se>
2021-05-07 10:29:56 +0200

    Jag ändrade mig och tog bort addition funktionen

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in); //Läser in input
         String s = in.next();
-        int a = in.nextInt();
-        int b = in.nextInt();
-        if (s.equals("add")) {
-            add(a,b);
-        }
-    }
-    private static void add(int a,int b) {
-        System.out.println(a+b);
+
     }
 }
