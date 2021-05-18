0236f774d3d019e0a50f45ea533076a3e2b7e28b
Skalmanleif <isak.thyrel@elev.ga.ntig.se>
2021-05-18 11:06:49 +0200

    Ändrade från long till double

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in); //Läser in input
         String s="";
+        System.out.println("pq");
         while (!s.equals("quit")) {
             s = in.next();
             if (s.equals("pq")) {
@@ -22,18 +23,18 @@ public class Main {
 
     private static void triangleA(Scanner in) {
         System.out.println("triangleA: b,h");
-        long b = in.nextLong();
-        long h = in.nextLong();
+        double b = in.nextDouble();
+        double h = in.nextDouble();
         System.out.println((b*h)/2);
     }
 
     private static void pq(Scanner in) {
         System.out.println("pq: p,q");
-        long p = in.nextLong();
-        long q = in.nextLong();
-        long sqrt = (long) Math.sqrt(Math.pow(-p / 2, 2) - q);
-        long x1 = ((-p/2) + sqrt);
-        long x2 = ((-p/2) - sqrt);
+        double p = in.nextDouble();
+        double q = in.nextDouble();
+        double sqrt =  Math.sqrt(Math.pow(-p / 2, 2) - q);
+        double x1 = ((-p/2) + sqrt);
+        double x2 = ((-p/2) - sqrt);
         System.out.println("x1 = " + x1 + "\nx2 = " + x2);
     }
 }
